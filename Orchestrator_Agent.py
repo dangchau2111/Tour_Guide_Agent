8b1b8a4e46500e56f9425638c4cccf2a5c462fed
dangchau2111 <dangbaochau2111@gmail.com>
2026-02-25 00:31:50 +0700

    first commit

---
+++ Orchestrator_Agent.py
@@ -0,0 +1,87 @@
+import os
+import json
+from pydantic import BaseModel, ValidationError
+from typing import Optional
+from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
+
+# Setup LLM
+HF_TOKEN = os.getenv("HF_TOKEN")
+client = OpenAI(
+    base_url="https://router.huggingface.co/v1",
+    api_key=HF_TOKEN
+)
+
+class FoodExtraction(BaseModel):
+    # Optional[str] means this field can be a string or None (null in JSON)
+    type_of_food: Optional[str] 
+    
+    # str means this field MUST be a string and cannot be missing
+    filter_tags: str
+
+# Get system prompt
+with open("prompts\orchestrator_agent.txt", 'r', encoding='utf-8') as file:
+    system_prompt = file.read()
+    
+
+
+def get_route_json(user_prompt):
+    completion = client.chat.completions.create(
+        model="Qwen/Qwen2.5-7B-Instruct:together",
+        messages=[
+            {
+                "role": "system",
+                "content": system_prompt
+            },
+            {
+                "role": "user",
+                "content": user_prompt
+            }
+        ],
+    )
+    agent_response = completion.choices[0].message.content
+    return agent_response
+
+def validate_route_json(agent_response, current_prompt):
+    try:
+        FoodExtraction.model_validate_json(agent_response)
+        return {
+            "res" : "successfully"
+        }
+    
+    except ValidationError as e:
+        new_prompt = current_prompt + f"\nLưu ý đừng mắc lỗi sau nhé:\n{e}"
+        return {
+            "res" : "failed",
+            "user_prompt": new_prompt
+        }
+    
+    except Exception as e:
+        new_prompt = current_prompt + f"\nLưu ý đừng mắc lỗi sau nhé:\n{e}"
+        return {
+            "res" : "failed",
+            "user_prompt": new_prompt
+        }
+    
+def get_routing_from_orchestrator(user_prompt:str):
+
+    max_retries = 3
+    for attempt in range(max_retries):
+        # Get response from LLM
+        agent_response = get_route_json(user_prompt)
+        
+        # Validate the response
+        valid_status = validate_route_json(agent_response, current_prompt = user_prompt)
+        
+        # Check if validation passed
+        if valid_status["res"] == "successfully":
+            return agent_response # Return immediately if correct
+        else:
+            # If failed, update the prompt with the error message and retry
+            user_prompt = valid_status["user_prompt"]
+
+    # If it fails all 3 attempts, return the last generated response 
+    return agent_response
+
+# print(get_routing_from_orchestrator(user_prompt="Bạn có thể đề xuất cho tôi các món ăn trưa ở Qui Nhơn không?"))
---
+++ tools/get_food.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import difflib
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+data_path = os.getenv("data_path")
+
+def get_food_list(type_of_food : str, filter_tags : str):
+    df_food = pd.read_excel(data_path, sheet_name='am_thuc')
+
+    # Filter by type of food first
+    allowed_types = ["món chính", "món phụ", "đồ ăn vặt", "đồ tráng miệng", "đồ uống"]
+
+    if str(type_of_food).lower() in allowed_types:
+        type_of_food = str(type_of_food).lower()
+    else:
+        type_of_food = None
+
+    if type_of_food is not None:
+        df_filtered = df_food[df_food['loai_mon_an'].str.lower() == type_of_food].copy()
+    else:
+        df_filtered = df_food
+
+
+    # Filter by tags
+    filter_tags_lower = str(filter_tags).lower()
+    sim_scores_list = []
+
+    for tag in df_filtered["Tags"]:
+        tag_lower = str(tag).lower()
+
+        # Calculate similarity score (returns a float between 0.0 and 1.0)
+        score = difflib.SequenceMatcher(None, filter_tags_lower, tag_lower).ratio()
+
+        # Convert score to percentage for easier reading
+        score_percentage = round(score * 100, 2)
+
+        sim_scores_list.append(score_percentage)
+
+    df_filtered["sim_score"] = sim_scores_list
+
+    df_result = df_filtered.nlargest(5, 'sim_score').copy()
+
+    return df_result[["ten_mon_an", "Description"]]
+
+# print(get_food_list( type_of_food = "món chính", filter_tags = "ăn sáng"))
